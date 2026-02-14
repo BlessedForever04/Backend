83f1e0cfb99282ce9df0efc3215ca94c3f5afa14
Yash Khanande <yashkhanande7@gmail.com>
2026-02-14 13:46:45 +0530

    members entity changed

--- src/main/java/com/chitalebandhu/chitalebandhu/entity/Member.java
+++ src/main/java/com/chitalebandhu/chitalebandhu/entity/Member.java
@@ -12,7 +12,37 @@ public class Member {
     @Id
     private String id;
     private String name;
+
+    private String email;
+    private String mobileNo;
+    private String role;
     private List<String> tasks = new ArrayList<>();
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobileNo() {
+        return mobileNo;
+    }
+
+    public void setMobileNo(String mobileNo) {
+        this.mobileNo = mobileNo;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+
 
     public String getId() {
         return id;
